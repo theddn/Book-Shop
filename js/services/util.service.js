7317898df8cc8dad9b865a3123309d78f34ab903
theddn <theddn@gmail.com>
2024-02-05 11:48:50 +0200

    Add util services

---
+++ js/services/util.service.js
@@ -0,0 +1,16 @@
+'use strict'
+function makeId(length = 5) {
+	var id = ''
+	var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+	for (var i = 0; i < length; i++) {
+		id += possible.charAt(Math.floor(Math.random() * possible.length))
+	}
+	return id
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min)
+	max = Math.floor(max)
+	return Math.floor(Math.random() * (max - min) + 1 + min) // The maximum is exclusive and the minimum is inclusive
+}
